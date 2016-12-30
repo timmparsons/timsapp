0a23bd1015daaf374298b505c6706501605f7be4
Tim Parsons <tim.m.parsons@hotmail.com>
2016-12-29 16:29:59 -0800

    4.2 add color to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161229063502) do
+ActiveRecord::Schema.define(version: 20161230002148) do
 
   create_table "products", force: :cascade do |t|
     t.string   "namr"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161229063502) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end

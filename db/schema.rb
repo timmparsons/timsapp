48abcf95d248a2e510a3aedc0a77bbe01e7c2537
Tim Parsons <tim.m.parsons@hotmail.com>
2017-01-08 09:10:18 -0800

    uodated color migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104231152) do
-
-  create_table "orders", force: :cascade do |t|
-    t.string  "user_id"
-    t.integer "product_id"
-    t.float   "total"
-    t.index ["product_id"], name: "index_orders_on_product_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "colour"
-    t.integer  "price"
-  end
+ActiveRecord::Schema.define(version: 20170104194006) do
 
 end
